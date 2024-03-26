59f111717e4d1856ead19aa85cbae50a8dcd8d07
Seeba184 <seenasimkani@gmail.com>
2024-03-26 14:49:07 -0400

    feat: add name attribute to buttons

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const displayScreen = document.querySelector("#screen");
+const number = document.querySelector(".number");
+const operator = document.querySelector(".operator");
+
 let operateFunctions = {
   "+": (a, b) => a + b,
   "-": (a, b) => a - b,
@@ -13,3 +17,12 @@ let divide = "/";
 function operate(operator, num1, num2) {
   return operateFunctions[operator](num1, num2);
 }
+
+
+  number.addEventListener("click", () => {
+    displayScreen.textContent = number.getAttribute("name");
+  });
+  operator.addEventListener("click", () => {
+    displayScreen.textContent = operator.getAttribute("name");
+  });
+
