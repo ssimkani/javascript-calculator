62ce91132fb350b089547e9af28755d239d62f85
Seeba184 <seenasimkani@gmail.com>
2024-03-26 13:07:23 -0400

    feat: add operator object with functions and assign operator variables

---
+++ index.js
@@ -0,0 +1,11 @@
+let operators = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "x": (a, b) => a * b,
+  "/": (a, b) => a / b,
+};
+
+let add = "+";
+let subtract = "-";
+let multiply = "x";
+let divide = "/";
