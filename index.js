0616044313d79938bfe09cbd23da959cd4729431
Seeba184 <seenasimkani@gmail.com>
2024-03-27 11:41:35 -0400

    rework: adjust operator eventListener to include one counter

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function operate(operator, num1, num2) {
 let number1 = "";
 let number2 = "";
 let operator = "";
-let operatorCounter = 0;
 let numButtonCounter = 0;
 
 numberButton.forEach((button) => {
@@ -37,18 +36,20 @@ numberButton.forEach((button) => {
 
 operatorButton.forEach((btn) => {
   btn.addEventListener("click", () => {
-    operatorCounter += 1;
-    if (operatorCounter < 2) {
-      number1 = parseInt(displayScreen.textContent);
-      operator = btn.textContent;
-      numButtonCounter = 0;
-    } else {
-      number2 = parseInt(displayScreen.textContent);
-      displayScreen.textContent = String(operate(operator, number1, number2));
-      operator = btn.textContent;
-      number1 = parseInt(displayScreen.textContent);
-      operatorCounter = 1;
-      numButtonCounter = 0;
+    if (numButtonCounter > 0) {
+      if (operator !== "") {
+        operatorButton.disabled = true;
+        number2 = parseFloat(displayScreen.textContent);
+        displayScreen.textContent = String(operate(operator, number1, number2));
+        operator = btn.textContent;
+        number1 = parseInt(displayScreen.textContent);
+        numButtonCounter = 0;
+      } else {
+        btn.disabled = true;
+        number1 = parseFloat(displayScreen.textContent);
+        operator = btn.textContent;
+        numButtonCounter = 0;
+      }
     }
   });
 });
