7460265bbafb4cdf6c3769047d17f4a1d9f518d0
Seeba184 <seenasimkani@gmail.com>
2024-03-27 10:02:50 -0400

    feat: add counter to determine number of operations

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const displayScreen = document.querySelector("#screen");
 const numberButton = document.querySelectorAll(".number");
-const operator = document.querySelectorAll(".operator");
+const operatorButton = document.querySelectorAll(".operator");
 
 let operateFunctions = {
   "+": (a, b) => a + b,
@@ -18,9 +18,33 @@ function operate(operator, num1, num2) {
   return operateFunctions[operator](num1, num2);
 }
 
-numberButton.forEach((button) => {
-  button.addEventListener("click", () => {
-    let num = button.textContent;
-    displayScreen.textContent = num;
+let clicked = false;
+let counter = 0;
+displayScreen.textContent = "";
+if (!clicked) {
+  numberButton.forEach((button) => {
+    button.addEventListener("click", () => {
+      let num = button.textContent;
+      displayScreen.textContent += num;
+    });
+  });
+}
+
+operatorButton.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    clicked = true;
+    counter += 1
+    let number1 = displayScreen.textContent;
+    displayScreen.textContent = "";
+    let operator = operatorButton.textContent;
   });
 });
+
+if (clicked) {
+  numberButton.forEach((button) => {
+    button.addEventListener("click", () => {
+      let num = button.textContent;
+      displayScreen.textContent += num;
+    });
+  });
+}
