5019684abbc27098c4543c380ac5b1097265de32
Seeba184 <seenasimkani@gmail.com>
2024-03-27 11:20:01 -0400

    fix: update display to not clear after clicking operator

--- index.js
+++ index.js
@@ -20,30 +20,35 @@ function operate(operator, num1, num2) {
 let number1 = "";
 let number2 = "";
 let operator = "";
-let counter = 0;
-displayScreen.textContent = "";
+let operatorCounter = 0;
+let numButtonCounter = 0;
 
 numberButton.forEach((button) => {
   button.addEventListener("click", () => {
-    let num = button.textContent;
-    displayScreen.textContent += num;
+    if (numButtonCounter === 0) {
+      displayScreen.textContent = button.textContent;
+      numButtonCounter += 1;
+    } else {
+      let num = button.textContent;
+      displayScreen.textContent += num;
+    }
   });
 });
 
 operatorButton.forEach((btn) => {
   btn.addEventListener("click", () => {
-    counter += 1;
-    if (counter < 2) {
+    operatorCounter += 1;
+    if (operatorCounter < 2) {
       number1 = parseInt(displayScreen.textContent);
       operator = btn.textContent;
-      displayScreen.textContent = "";
+      numButtonCounter = 0;
     } else {
-      let number2 = parseInt(displayScreen.textContent);
+      number2 = parseInt(displayScreen.textContent);
       displayScreen.textContent = String(operate(operator, number1, number2));
       operator = btn.textContent;
       number1 = parseInt(displayScreen.textContent);
-      counter = 1;
-      displayScreen.textContent = ""
+      operatorCounter = 1;
+      numButtonCounter = 0;
     }
   });
 });
